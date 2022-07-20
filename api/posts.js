7576966c57bdbcb15997ee9d2574434a84669083
alina <sapron4ik93@gmail.com>
2022-07-20 13:25:53 -0500

    done

--- api/posts.js
+++ api/posts.js
@@ -73,13 +73,57 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
     }
   });
 
-postsRouter.get("/", async (req, res) => {
-  const posts = await getAllPosts();
-
-  res.send({
-    posts,
+  postsRouter.delete('/:postId', requireUser, async (req, res, next) => {
+    try {
+      const post = await getPostById(req.params.postId);
+  
+      if (post && post.author.id === req.user.id) {
+        const updatedPost = await updatePost(post.id, { active: false });
+  
+        res.send({ post: updatedPost });
+      } else {
+        // if there was a post, throw UnauthorizedUserError, otherwise throw PostNotFoundError
+        next(post ? { 
+          name: "UnauthorizedUserError",
+          message: "You cannot delete a post which is not yours"
+        } : {
+          name: "PostNotFoundError",
+          message: "That post does not exist"
+        });
+      }
+  
+    } catch ({ name, message }) {
+      next({ name, message })
+    }
   });
-});
+
+
+  postsRouter.get('/', async (req, res, next) => {
+    try {
+      const allPosts = await getAllPosts();
+  
+      const posts = allPosts.filter(post => {
+        if (post.active) {
+            return true;
+          }
+        
+          // the post is not active, but it belogs to the current user
+          if (req.user && post.author.id === req.user.id) {
+            return true;
+          }
+        
+          // none of the above are true
+          return false;
+        // keep a post if it is either active, or if it belongs to the current user
+      });
+  
+      res.send({
+        posts
+      });
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  });
 
 
 module.exports = postsRouter;
--- api/tags.js
+++ api/tags.js
@@ -22,12 +22,29 @@ tagsRouter.get('/', async (req, res) => {
     // read the tagname from the params
     try {
         
-       const posts = await getPostsByTagName(tagName);
+       const allPosts = await getPostsByTagName(tagName);
+      
+        const posts = allPosts.filter(post => {
+           
+        if (post.active) {
+
+            return true;
+          }
+        
+        //   the post is not active, but it belogs to the current user
+          if (req.user && post.author.id === req.user.id) {
+            return true;
+          }
+        
+          // none of the above are true
+          return false;
+        // keep a post if it is either active, or if it belongs to the current user
+      });
       // use our method to get posts by tag name from the db
       res.send({posts:posts})
       // send out an object to the client { posts: // the posts }
     } catch ({ name, message }) {
-        
+
         next({ name, message });
       // forward the name and message to the error handler
     }
