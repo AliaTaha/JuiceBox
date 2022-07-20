78dc6bfb0b2368f4789165c964b96c7bd1466f7a
rafaelbchs <rafaelbchacins@gmail.com>
2022-07-20 10:57:38 -0500

    made post requests

--- api/posts.js
+++ api/posts.js
@@ -9,8 +9,33 @@ postsRouter.use((req, res, next) => {
   next(); // THIS IS DIFFERENT
 });
 
-postsRouter.post("/", requireUser, async (req, res, next) => {
-  res.send({ message: "under construction" });
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+  console.log("this is the body", req.body)
+  console.log("this is the user", req.user)
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    postData.authorId = req.user.id
+    postData.title = title
+    postData.content = content
+    const post = await createPost(postData);
+    if(post){
+      res.send({ post })
+    }
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object 
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 postsRouter.get("/", async (req, res) => {
